77619981b9cb3a27004626d8d0e0d4589a152166
berglind-halldorsdottir <90624284+berglind-halldorsdottir@users.noreply.github.com>
2021-09-23 15:22:19 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@ import sys
 from pathlib import Path
 from sphinx.ext.autodoc import between
 
-na_analytics_src = "/".join(Path(__file__)._parts[1:-3] +['src'])
-sys.path.insert(0, na_analytics_src)
+#na_analytics_src = "/".join(Path(__file__)._parts[1:-3] +['src'])
+#sys.path.insert(0, na_analytics_src)
 
 
 # -- Project information -----------------------------------------------------
@@ -65,4 +65,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
