11108d158d3d20b28bbe94a1d59ab3f12bc92643
berglind-halldorsdottir <90624284+berglind-halldorsdottir@users.noreply.github.com>
2021-09-24 15:15:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,8 @@ else:
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-autoapi_ignore = ["*/tests/*",
-                  "*noxfile.py"]
+autoapi_ignore = ["*tests*",
+                  "*/noxfile.py"]
 
 # add_function_parentheses = True
 
