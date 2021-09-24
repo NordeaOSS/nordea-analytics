a4de2d955bef43af6a014579541a8b1b61362afe
berglind-halldorsdottir <90624284+berglind-halldorsdottir@users.noreply.github.com>
2021-09-24 14:44:38 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,12 +52,8 @@ else:
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-autoapi_ignore = ["*/tests/*",
-                  "*noxfile.py", "*conf.py"]
+autoapi_ignore = ["tests/*",
+                  "noxfile.py"]
 
 # add_function_parentheses = True
 
