e3c916f417711013eac6ea0efb308c123524657c
berglind-halldorsdottir <90624284+berglind-halldorsdottir@users.noreply.github.com>
2021-10-29 10:00:05 +0200

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-"""Shared fixtures and test configuration."""
-
-from _pytest.config import Config
-
-
-def pytest_configure(config: Config) -> None:
-    """Configure pytest."""
-    config.addinivalue_line("markers", "e2e: mark as end-to-end tests.")
